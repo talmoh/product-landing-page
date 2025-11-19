df2618ab7eef5741378b49d02f5cb2e2e48e8c75
Bureau2025! <talbi.mohamed.bureau@gamil.com>
2025-11-19 14:12:15 +0100

    ajout speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { CartProvider } from '../context/CartContext'
 import Navbar from '../components/Navbar';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Navbar />
           {children}
           <Analytics />
+          <SpeedInsights />
         </CartProvider>
       </body>
     </html>
